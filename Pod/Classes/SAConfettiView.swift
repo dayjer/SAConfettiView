c39d342aa783e9ca4cff09d286cd7870c92f3acb
Jeremy Day <jeremy@burleighcreative.com>
2017-11-06 17:14:08 +1000

    attempt at exposing some variables to objc

--- Pod/Classes/SAConfettiView.swift
+++ Pod/Classes/SAConfettiView.swift
@@ -20,34 +20,24 @@ import QuartzCore
   }
   
   var emitter: CAEmitterLayer!
-  @objc var colors: [UIColor]!
-  @objc var intensity: CGFloat
-  var type: ConfettiType!
-  private var active :Bool!
+  @objc var colors: [UIColor] = [UIColor(red:0.95, green:0.40, blue:0.27, alpha:1.0),
+                                 UIColor(red:1.00, green:0.78, blue:0.36, alpha:1.0),
+                                 UIColor(red:0.48, green:0.78, blue:0.64, alpha:1.0),
+                                 UIColor(red:0.30, green:0.76, blue:0.85, alpha:1.0),
+                                 UIColor(red:0.58, green:0.39, blue:0.55, alpha:1.0)]
+  @objc var intensity: CGFloat = 0.5
+  var type: ConfettiType = .Confetti
+  private var active :Bool = false
   
   required public init?(coder aDecoder: NSCoder) {
-    intensity = 0.5
     super.init(coder: aDecoder)
-    setup()
   }
   
   public override init(frame: CGRect) {
-    intensity = 0.5
-
     super.init(frame: frame)
-    setup()
   }
   
-  func setup() {
-    colors = [UIColor(red:0.95, green:0.40, blue:0.27, alpha:1.0),
-              UIColor(red:1.00, green:0.78, blue:0.36, alpha:1.0),
-              UIColor(red:0.48, green:0.78, blue:0.64, alpha:1.0),
-              UIColor(red:0.30, green:0.76, blue:0.85, alpha:1.0),
-              UIColor(red:0.58, green:0.39, blue:0.55, alpha:1.0)]
-    intensity = 0.5
-    type = .Confetti
-    active = false
-  }
+
   
   @objc public func startConfetti() {
     emitter = CAEmitterLayer()
